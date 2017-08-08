65861a6662868f7692dc4e5cd12ec75dc16c249d
lasid <lasid@debian>
2017-08-08 00:28:44 -0300

    Change port number

--- app1/app.py
+++ app1/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Flask Dockerized'
+    return 'Flask Dockerized App1'
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5001)
